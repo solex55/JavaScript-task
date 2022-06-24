ccd26729ca8ce8a53bebf89c8170456ce920c1d1
Solomon Achugwoh <52291436+solex55@users.noreply.github.com>
2022-06-24 15:36:54 +0100

    Add index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+var myName = 'Solomon Achugwoh';
+
+var height = '5ft9in';
+
+var country = 'Nigeria';
+
+var newline = "\r\n";
+
+ alert("My name is " + myName + newline + "I'm " + height + " " + "tall"+ newline + "I'm from " + country);
+
+document.write("My name is " + myName + "<br>"+"<br>");
+
+document.write("I'm " + height + " " + "tall" + "<br>"+"<br>");
+
+document.write("I'm from " + country + "<br>" + "<br>");
+
